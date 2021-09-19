6ad148391f23704339f7c9c1f08846f8f3ae1d0a
kettunenj <juhani.kettunen@cgi.com>
2021-09-19 20:47:53 +0300

    Added jwt validation and parsing

---
+++ src/main/java/fi/liike/rest/auth/JwtRequestFilter.java
@@ -0,0 +1,135 @@
+package fi.liike.rest.auth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.SignatureException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+public class JwtRequestFilter {
+    private final Logger LOG = LoggerFactory.getLogger(JwtRequestFilter.class);
+
+    private static String publicKey = null;
+    private static PublicKey ecPublicKey = null;
+
+    private String getPublicKey(String kid, boolean isForce) throws Exception {
+        if (isForce || publicKey == null) {
+            String url = "https://public-keys.auth.elb.eu-west-1.amazonaws.com/" + kid;
+            HttpsURLConnection httpClient = (HttpsURLConnection) new URL(url).openConnection();
+            httpClient.setRequestMethod("GET");
+            httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+            int responseCode = httpClient.getResponseCode();
+
+            LOG.debug(String.format("Sending request %s", url));
+            LOG.debug(String.format("Response code %s", responseCode));
+
+            String key = "";
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(httpClient.getInputStream()))) {
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+                key = response.toString()
+                        .replaceAll("-----BEGIN PUBLIC KEY-----", "")
+                        .replaceAll("-----END PUBLIC KEY-----", "");
+
+                LOG.debug(String.format("Public key %s", key));
+            }
+            publicKey = key;
+            ecPublicKey = null;
+        }
+        return publicKey;
+    }
+
+    private Claims decodeJWT(String jwt, String key) throws Exception {
+        if (ecPublicKey == null) {
+            byte[] publicKeyBytes = Base64.getDecoder().decode(key);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("EC");
+            ecPublicKey = keyFactory.generatePublic(keySpec);
+        }
+        return Jwts.parserBuilder().setSigningKey(ecPublicKey).build().parseClaimsJws(jwt).getBody();
+    }
+
+    protected List<UserGroup> getUserGroups(HttpServletRequest request) throws ServletException, IOException {
+        List<UserGroup> userGroups = new ArrayList<UserGroup>();
+        try {
+            LOG.debug(String.format("Path %s", request.getServletPath()));
+            
+
+            String jwt = request.getHeader("x-amzn-oidc-data");
+            if (jwt == null || jwt.length() == 0) {
+                jwt = request.getHeader("x-iam-data");
+            }
+            if (jwt != null) {
+                String jwt_headers = jwt.split("\\.")[0];
+                String decoded_jwt_headers = new String(Base64.getDecoder().decode(jwt_headers));
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(decoded_jwt_headers);
+
+                // logger.debug(String.format("JWT headers json %s", json.toJSONString()));
+
+                String key = getPublicKey(jsonNode.get("kid").asText(),false);
+                Claims claims;
+                try {
+                    claims = decodeJWT(jwt, key);
+                } catch (SignatureException e) {
+                    LOG.debug("Invalid key, trying again");
+                    key = getPublicKey(jsonNode.get("kid").asText(),true);
+                    claims = decodeJWT(jwt, key);
+                }
+
+                if (claims != null) {
+                    String username = (String) claims.get("username");
+                    String uid = (String) claims.get("custom:uid");
+                    String userNameDetail = (uid != null) ? uid : username;
+                    LOG.debug(String.format("Username %s", userNameDetail));
+
+                    String[] roles = ((String) claims.get("custom:rooli")).split("\\,");
+                    LOG.debug(String.format("Roles %s", String.join(",", roles)));
+
+                    // claims.forEach((k, v) -> LOG.debug(String.format("Claim %s=%s", k, v)));
+                    
+                    for(String role:roles){
+                        switch (role) {
+                            case "tk_tietoturva":
+                                userGroups.add(UserGroup.SUPER_USER);
+                            case "tk_muokkaus":
+                                userGroups.add(UserGroup.MODIFY_USER);
+                            default:
+                                LOG.error("JWT tokenissa ei ollut yhtaan tunnettua roolia");
+                        }
+                    }
+                }
+            } else {
+                LOG.error("No JWT header found");
+            }
+            return userGroups;
+        } catch (Exception ex) {
+            LOG.error("Autorisointi epaonnistui", ex);
+            return userGroups;
+        }
+    }
+}
