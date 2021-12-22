205ee78ab092b5a278e36d80fdc994fe751351d4
Alvar Wihuri <alvar.wihuri@cgi.com>
2021-12-22 12:11:47 +0200

    Fixed view creation

--- src/main/resources/db/src/sql/database/postgres2/03_missing_data.sql
+++ src/main/resources/db/src/sql/database/postgres2/03_missing_data.sql
@@ -1,22 +1,23 @@
-
--- Helper view that shows tietolaji and tietoryhma values related to jarjestelma entities.
-
-  CREATE OR REPLACE FORCE VIEW "TIETOK"."JARJESTELMA_TIETOARKKITEHTUURI" ("JARJESTELMAN_NIMI", "TIETOJARJESTELMATUNNUS", "TIETONIMI", "TIETOTUNNUS", "TIETORYHMANIMI", "TIETORYHMATUNNUS") AS
-  SELECT
+-- TKYP-139 Tietojärjestelmäpalvelun muutokset
+-- Modify the view to use JARJESTELMALOOGINEN join table instead of LOOGINENTIETOVARANTO and its KOODI field.
+CREATE OR REPLACE VIEW TIETOK.JARJESTELMA_TIETOARKKITEHTUURI
+            (JARJESTELMAN_NIMI,TIETOJARJESTELMATUNNUS,TIETONIMI,TIETOTUNNUS,TIETORYHMANIMI,TIETORYHMATUNNUS)
+AS
+SELECT
     tietojarjestelmasalkku.jarjestelman_nimi, tietojarjestelmasalkku.tietojarjestelmatunnus, tieto.nimi AS tietonimi, tieto.tietotunnus, tietoryhma.nimi AS tietoryhmanimi, tietoryhma.tietoryhmatunnus
 FROM
-    tietojarjestelmasalkku
-JOIN looginentietovaranto ON
-    tietojarjestelmasalkku.tietojarjestelmatunnus = looginentietovaranto.koodi
-JOIN tieto ON
-    looginentietovaranto.looginentietovarantotunnus = tieto.looginentietovarantotunnus
-LEFT JOIN tietoryhma ON
-    tieto.tietoryhmatunnus = tietoryhma.tietoryhmatunnus
-;
-
+    tietok.tietojarjestelmasalkku
+        JOIN tietok.jarjestelmalooginen ON
+            tietojarjestelmasalkku.tietojarjestelmatunnus = jarjestelmalooginen.jarjestelmatunnus
+        JOIN tietok.looginentietovaranto ON
+            jarjestelmalooginen.looginentunnus = looginentietovaranto.looginentietovarantotunnus
+        JOIN tietok.tieto ON
+            tietok.looginentietovaranto.looginentietovarantotunnus = tieto.looginentietovarantotunnus
+        LEFT JOIN tietok.tietoryhma ON
+            tieto.tietoryhmatunnus = tietoryhma.tietoryhmatunnus;
 
 -- TKYP-124 Tieto-omaisuus -lomake
-CREATE VIEW TIETOOMAISUUS_JARJESTELMA_NIMI
+CREATE VIEW tietok.TIETOOMAISUUS_JARJESTELMA_NIMI
 AS select tietoomaisuus.*, tietojarjestelmasalkku.jarjestelman_nimi
-from tietoomaisuus
-join tietojarjestelmasalkku on (tietoomaisuus.tietojarjestelma_id = tietojarjestelmasalkku.tietojarjestelmatunnus);
+from tietok.tietoomaisuus
+join tietok.tietojarjestelmasalkku on (tietoomaisuus.tietojarjestelma_id = tietojarjestelmasalkku.tietojarjestelmatunnus);
--- src/main/resources/db/src/sql/h2_database/pg/03_missing_data.sql
+++ src/main/resources/db/src/sql/h2_database/pg/03_missing_data.sql
@@ -1,19 +1,22 @@
-
 -- Helper view that shows tietolaji and tietoryhma values related to jarjestelma entities.
 
-  CREATE OR REPLACE FORCE VIEW "TIETOK"."JARJESTELMA_TIETOARKKITEHTUURI" ("JARJESTELMAN_NIMI", "TIETOJARJESTELMATUNNUS", "TIETONIMI", "TIETOTUNNUS", "TIETORYHMANIMI", "TIETORYHMATUNNUS") AS
-  SELECT
+-- TKYP-139 Tietojärjestelmäpalvelun muutokset
+-- Modify the view to use JARJESTELMALOOGINEN join table instead of LOOGINENTIETOVARANTO and its KOODI field.
+CREATE OR REPLACE VIEW JARJESTELMA_TIETOARKKITEHTUURI
+            (JARJESTELMAN_NIMI,TIETOJARJESTELMATUNNUS,TIETONIMI,TIETOTUNNUS,TIETORYHMANIMI,TIETORYHMATUNNUS)
+AS
+SELECT
     tietojarjestelmasalkku.jarjestelman_nimi, tietojarjestelmasalkku.tietojarjestelmatunnus, tieto.nimi AS tietonimi, tieto.tietotunnus, tietoryhma.nimi AS tietoryhmanimi, tietoryhma.tietoryhmatunnus
 FROM
     tietojarjestelmasalkku
-JOIN looginentietovaranto ON
-    tietojarjestelmasalkku.tietojarjestelmatunnus = looginentietovaranto.koodi
-JOIN tieto ON
-    looginentietovaranto.looginentietovarantotunnus = tieto.looginentietovarantotunnus
-LEFT JOIN tietoryhma ON
-    tieto.tietoryhmatunnus = tietoryhma.tietoryhmatunnus
-;
-
+        JOIN jarjestelmalooginen ON
+            tietojarjestelmasalkku.tietojarjestelmatunnus = jarjestelmalooginen.jarjestelmatunnus
+        JOIN looginentietovaranto ON
+            jarjestelmalooginen.looginentunnus = looginentietovaranto.looginentietovarantotunnus
+        JOIN tieto ON
+            looginentietovaranto.looginentietovarantotunnus = tieto.looginentietovarantotunnus
+        LEFT JOIN tietoryhma ON
+            tieto.tietoryhmatunnus = tietoryhma.tietoryhmatunnus;
 
 -- TKYP-124 Tieto-omaisuus -lomake
 CREATE VIEW TIETOOMAISUUS_JARJESTELMA_NIMI
