e4ef3cfb35686f1748cafaf67586a5bd6f39aa76
Alvar Wihuri <alvar.wihuri@cgi.com>
2022-09-30 15:55:39 +0300

    Use BeanUtils in JarjestelmaConverter and TermilomakeConverter

--- src/main/java/fi/liike/rest/api/Converter/JarjestelmaConverter.java
+++ src/main/java/fi/liike/rest/api/Converter/JarjestelmaConverter.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static fi.liike.rest.util.Utils.dateToUrlParam;
 
-public class JarjestelmaConverter implements MinimalConverter {
+public class JarjestelmaConverter extends BasicConverter implements MinimalConverter {
 
 	public JoinJarjestelmaLinkkaus jarjestelmaLinkkausDtoToDomain(JarjestelmaLinkkausDto jarjestelmaLinkkaus) {
 		JoinJarjestelmaLinkkaus joinJarjestelmaLinkkaus = new JoinJarjestelmaLinkkaus();
@@ -193,14 +193,14 @@ public class JarjestelmaConverter implements MinimalConverter {
 
 	@Override
 	public ContentDto modelToDto(Haettava modelObject, Integer... parentId) {
-		return modelToDto(modelObject, null, null, parentId);
+		return modelToDto(modelObject, null, null);
 	}
 
 	public ContentDto modelToDto(Haettava modelObject, List<FetchHenkiloRooliDto> fetchHenkiloRooliDtoList,
 								 List<JoinJarjestelmaLinkkaus> joinJarjestelmaLinkkausList,
 								 MolekyyliLinkkiDto linkData, Integer... parentId) {
 
-		JarjestelmaDto jarjestelmaDto = (JarjestelmaDto) modelToDto(modelObject, joinJarjestelmaLinkkausList, linkData, parentId);
+		JarjestelmaDto jarjestelmaDto = (JarjestelmaDto) modelToDto(modelObject, joinJarjestelmaLinkkausList, linkData);
 
 		if (jarjestelmaDto != null) {
 			jarjestelmaDto.setFetchRooliHenkiloList(fetchHenkiloRooliDtoList);
@@ -209,88 +209,20 @@ public class JarjestelmaConverter implements MinimalConverter {
 	}
 
 	private ContentDto modelToDto(Haettava modelObject, List<JoinJarjestelmaLinkkaus> joinJarjestelmaLinkkausList,
-								  MolekyyliLinkkiDto linkData, Integer... parentId) {
+								  MolekyyliLinkkiDto linkData) {
 		if (modelObject == null)
 			return null;
 		Jarjestelma data = (Jarjestelma) modelObject;
 		JarjestelmaDto result = new JarjestelmaDto();
-		result.setTunnus(data.getTunnus());
-		result.setNimi(data.getNimi());
-		result.setKuvaus(data.getKuvaus());
-		result.setJarjestelmatyyppi(data.getJarjestelmatyyppi());
-		result.setLyhennetty_nimi(data.getLyhennetty_nimi());
-		result.setKayttajaryhmat(data.getKayttajaryhmat());
-		result.setElinkaaritila(data.getElinkaaritila());
-		result.setToimittaja(data.getToimittaja());
-		result.setToimittajan_yhteyshenkilo(data.getToimittajan_yhteyshenkilo());
-		result.setSisaiset_kayttajat(data.getSisaiset_kayttajat());
-		result.setLisenssitieto(data.getLisenssitieto());
-		result.setAlfresco_linkki(data.getAlfresco_linkki());
-		result.setKriittisyys(data.getKriittisyys());
-		result.setMuut_toimittajat(data.getMuut_toimittajat());
-		result.setVersio(data.getVersio());
-		result.setTietoj_kriittisyys_toiminnalle(data.getTietoj_kriittisyys_toiminnalle());
-		result.setTietovarastot(data.getTietovarastot());
-		result.setPaasynhallinta(data.getPaasynhallinta());
-		result.setPortaalipalvelu(data.getPortaalipalvelu());
-		result.setTunnistetut_kehitystarpeet(data.getTunnistetut_kehitystarpeet());
-		result.setToteutusteknologia(data.getToteutusteknologia());
-		result.setSisaisten_kayttajien_maara(data.getSisaisten_kayttajien_maara());
-		result.setPalvelin(data.getPalvelin());
-		result.setTekninen_elinkaari(data.getTekninen_elinkaari());
-		result.setKayttooikeuksien_hallinta(data.getKayttooikeuksien_hallinta());
-		result.setKayttoonottovuosi(data.getKayttoonottovuosi());
-		result.setHuomautus_esimerkki(data.getHuomautus_esimerkki());
-		result.setKehityspanos_arvioitu(data.getKehityspanos_arvioitu());
-		result.setKehityskulut_arvioidut(data.getKehityskulut_arvioidut());
-		result.setPalveluaika(data.getPalveluaika());
-		result.setLiik_turvallisuusluokka(data.getLiik_turvallisuusluokka());
-		result.setTunnistetut_riskit(data.getTunnistetut_riskit());
+		super.convert(data, result);
 		result.setLuokituksen_tarkastuspvm(Utils.dateToStr(data.getLuokituksen_tarkastuspvm()));
-		result.setTietoturvatasoluokitus(data.getTietoturvatasoluokitus());
-		result.setIct_varautumisen_luokitus(data.getIct_varautumisen_luokitus());
-		result.setSla_luokitus_jhs(data.getSla_luokitus_jhs());
-		result.setSla_luokitus_kayttopalvelu(data.getSla_luokitus_kayttopalvelu());
-		result.setJarjestelman_tarkeys_liville(data.getJarjestelman_tarkeys_liville());
-		result.setJarj_tarkeys_yhteistyokump(data.getJarj_tarkeys_yhteistyokump());
-		result.setTurvallisuuskuvaus_laadittu(data.getTurvallisuuskuvaus_laadittu());
-		result.setToipumissuunnitelma_laadittu(data.getToipumissuunnitelma_laadittu());
-		result.setKehitystarve(data.getKehitystarve());
-		result.setKayttotiheys(data.getKayttotiheys());
-		result.setUlkoiset_kayttajat(data.getUlkoiset_kayttajat());
-		result.setPoistovuosi(data.getPoistovuosi());
-		result.setTietolahteet(data.getTietolahteet());
-		result.setSalassa_pidettavat_tiedot(data.getSalassa_pidettavat_tiedot());
-		result.setJulkiset_tiedot_ryhmittain(data.getJulkiset_tiedot_ryhmittain());
-		result.setTietoj_internet_osoite(data.getTietoj_internet_osoite());
-		result.setTietojen_julkisuus(data.getTietojen_julkisuus());
-		result.setRekisteriseloste(data.getRekisteriseloste());
-		result.setToimittajan_tekn_yh(data.getToimittajan_tekn_yh());
-		result.setYhteisk_kriit_jarj(data.getYhteisk_kriit_jarj());
-		result.setJarj_tarkeys_yk(data.getJarj_tarkeys_yk());
-		result.setJarjestelmaalue(data.getJarjestelmaalue());
 		result.setTietoturvasopimus((data.getTietoturvasopimus() == null) ? null : data.getTietoturvasopimus() == 1);
-		List<JarjestelmaLinkkausDto> jarjestelmaLinkkausList = new ArrayList<JarjestelmaLinkkausDto>();
+		List<JarjestelmaLinkkausDto> jarjestelmaLinkkausList = new ArrayList<>();
 		for (JoinJarjestelmaLinkkaus jarjestelmaLinkkaus : joinJarjestelmaLinkkausList) {
 			jarjestelmaLinkkausList.add(jarjestelmaLinkkausModelToDto(jarjestelmaLinkkaus));
 		}
 		result.setJarjestelmaLinkkausList(jarjestelmaLinkkausList);
 		result.setLinkData(linkData);
-		result.setConfluence_linkki(data.getConfluence_linkki());
-		result.setKayttopalvelun_toimittaja(data.getKayttopalvelun_toimittaja());
-		result.setOmistava_organisaatio(data.getOmistava_organisaatio());
-		result.setBudjetti(data.getBudjetti());
-		result.setRahoitusmomentti(data.getRahoitusmomentti());
-		result.setSalassapidon_peruste(data.getSalassapidon_peruste());
-		result.setTurvallisuusluokitus(data.getTurvallisuusluokitus());
-		result.setTarkeinta(data.getTarkeinta());
-		result.setTiedonluottamuksellisuus(data.getTiedonluottamuksellisuus());
-		result.setTiedon_saatavuus(data.getTiedon_saatavuus());
-		result.setTiedon_eheys(data.getTiedon_eheys());
-		result.setTarvitaan_viikonloppuna(data.getTarvitaan_viikonloppuna());
-		result.setTarvitaan_audit_trail(data.getTarvitaan_audit_trail());
-		result.setIntegraatioita_muihin(data.getIntegraatioita_muihin());
-
 		return result;
 	}
 
--- src/main/java/fi/liike/rest/api/Converter/TermilomakeConverter.java
+++ src/main/java/fi/liike/rest/api/Converter/TermilomakeConverter.java
@@ -2,25 +2,18 @@ package fi.liike.rest.api.Converter;
 
 import fi.liike.rest.Model.Haettava;
 import fi.liike.rest.Model.Termilomake;
-import fi.liike.rest.Model.TermilomakeJoinHierarkkinenKasite;
 import fi.liike.rest.api.ContentDto;
 import fi.liike.rest.api.dto.TermilomakeDto;
-import fi.liike.rest.api.dto.TermilomakeHierarkkinenKasiteDto;
-import org.apache.commons.beanutils.BeanUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class TermilomakeConverter implements Converter {
-    private final Logger LOG = LoggerFactory.getLogger(TermilomakeConverter.class);
+public class TermilomakeConverter extends BasicConverter implements Converter {
 
     @Override
     public Haettava dtoToDomain(ContentDto dtoContent) {
         TermilomakeDto termilomakeDto = (TermilomakeDto) dtoContent;
         Termilomake termilomake = new Termilomake();
-        convert(termilomakeDto, termilomake, true);
+        super.convert(termilomakeDto, termilomake);
         return termilomake;
     }
 
@@ -30,7 +23,7 @@ public class TermilomakeConverter implements Converter {
             return null;
         Termilomake termilomake = (Termilomake) modelObject;
         TermilomakeDto termilomakeDto = new TermilomakeDto();
-        convert(termilomakeDto, termilomake, false);
+        super.convert(termilomake, termilomakeDto);
         return termilomakeDto;
     }
 
@@ -49,25 +42,4 @@ public class TermilomakeConverter implements Converter {
         return dtoContent;
     }
 
-    // TODO: same as in tietovaranto converter! Refactor to a shared location
-    private <T, U> void convert(T dto, U model, Boolean toModel) {
-        try {
-            if (toModel)
-                BeanUtils.copyProperties(model, dto);
-            else
-                BeanUtils.copyProperties(dto, model);
-
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            LOG.error("Could not copy properties to BeanUtils! Error Message: " + e.getMessage());
-        }
-    }
-
-    public TermilomakeJoinHierarkkinenKasite joinHierarkkinenDtoToDomain(TermilomakeHierarkkinenKasiteDto dto) {
-        TermilomakeJoinHierarkkinenKasite ylaKasiteModel = new TermilomakeJoinHierarkkinenKasite();
-        ylaKasiteModel.setRivitunnus(dto.getId());
-        ylaKasiteModel.setChildNode(dto.getHierarkkinenAlakasite());
-        ylaKasiteModel.setParentNode(dto.getHierarkkinenYlakasite());
-        ylaKasiteModel.setRivitila("A");
-        return ylaKasiteModel;
-    }
 }
